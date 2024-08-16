bf08ba9857ec6ca4909a76b9cdac863301d035a7
Loren25-p <lorenopetrosyano@gmail.com>
2024-08-16 10:27:06 +0500

    homework

--- splide.js
+++ splide.js
@@ -6,4 +6,23 @@
     } );
     
     splide.mount();
+
+    let lessons = [
+      "Знакомство с html, css, js и разница между ними",
+      "Виды сайтов т первые терминалы",
+      "CSS - базовые стили и их подключение. Палитра",
+      "HTML [Таблицы]",
+      "HTML [Формы]",
+      "CSS [Intermediate]",
+    ]
+
+    lessons[1] = "Первый лендинг"
+    lessons.push("css Flex")
+
+    let ul = document.getElementsByClassName('lessons') [0]
+
+    for(let i = 0; i < lessons.length; i++) {
+      let(lesson = lessons[i])
+      ul.innerHTML = ul.innerHTML + `<li class="lesson">Урок №$(1+1): ${lesson}</li>`
+    }
     
